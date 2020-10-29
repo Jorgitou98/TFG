5437712844fe294d4a1a33fd26f61dd71aa4011e
Jorge Villarrubia <georgeatletico@gmail.com>
2020-10-29 17:45:08 +0100

    Add files via upload

--- PruebaGenerador.cpp
+++ PruebaGenerador.cpp
@@ -4,28 +4,37 @@
 #include <algorithm>
 #include <fstream>
 #include <string>
+#include <set>
+#include <iterator>
 
-#define MAXCIRCUITOS 10000
+#define MAXCIRCUITOS 1000000
+#define REPERTORIOUSADO 'A'
 
-std::ofstream fs("ficheroSalida" + std::to_string(MAXCIRCUITOS) + ".txt");
+std::ofstream fs("ficheroSalida" + std::to_string(MAXCIRCUITOS) + REPERTORIOUSADO + ".txt");
 unsigned int circuitosGenerados = 0;
 
-// Las entradas por comodidad tambien se han metido con las puertas
-enum puerta { AND, OR, NOT, NAND, NOR, XOR, XNOR, X0, X1, X2, X3, X4 };
+// Las entradas, por comodidad tambien se han metido con las puertas
+enum puerta { AND, OR, NOT, NAND, NOR, XOR, XNOR, LONLY, RONLY, NLONLY, NRONLY, X0, X1, X2, X3, X4 };
 
 
-std::vector<puerta> repertorioA = { NAND };
+std::vector<puerta> repertorioABinario = { NAND };
+std::vector<puerta> repertorioAUnario = {};
+
 std::vector<puerta> repertorioBBinario = { AND, OR };
 std::vector<puerta> repertorioBUnario = { NOT };
-//std::vector<char> repertorioC = { 'a', 'o', 'n' };
+
+std::vector<puerta> repertorioCBinario = { LONLY, RONLY, NLONLY, NRONLY, NOR, XOR, NAND, AND, XNOR, OR};
+std::vector<puerta> repertorioCUnario = {NOT};
+
+
 
 struct circuito {
 	unsigned int eval;
 	puerta gate;
 	circuito* padreIzq;
 	circuito* padreDer;
-	unsigned int tam;
-	unsigned int prof;
+	int tam;
+	int prof;
 	circuito(puerta gate, circuito* padreIzq, circuito* padreDer, unsigned int eval, unsigned int tam, unsigned int prof) {
 		this->gate = gate;
 		this->padreIzq = padreIzq;
@@ -64,6 +73,30 @@ std::ostream& operator<<(std::ostream& o, puerta const& gate) {
 	case NOT:
 		o << "NOT";
 		break;
+	case LONLY:
+		o << "LO";
+		break;
+	case RONLY:
+		o << "RO";
+		break;
+	case NLONLY:
+		o << "NLO";
+		break;
+	case NRONLY:
+		o << "NRO";
+		break;
+	case NOR:
+		o << "NOR";
+		break;
+	case XOR:
+		o << "XOR";
+		break;
+	case NAND:
+		o << "NAND";
+		break;
+	case XNOR:
+		o << "XNOR";
+		break;
 	default:
 		break;
 	}
@@ -71,7 +104,7 @@ std::ostream& operator<<(std::ostream& o, puerta const& gate) {
 }
 
 
-// Pinta el circuito como arbol binario
+// Pinta el circuito como árbol binario
 void printBT(std::ostream& o, const std::string& prefix, const circuito* circ, bool isLeft)
 {
 	if (circ != nullptr)
@@ -108,13 +141,33 @@ std::ostream& operator<<(std::ostream& o, circuito* const& circ) {
 }
 
 // Mapa de tamaños a circuitos de ese tamaño calculados que deben ser anotados
-std::unordered_map<unsigned int, std::vector<circuito*>> espera;
+std::unordered_map<int, std::vector<circuito*>> espera;
 
 // Mapa de funciones computadas a vector de circuitos que la computan (de izquierda a derecha creciente de tamaño)
 std::unordered_map<unsigned int, std::vector<circuito*>> almacen;
 
-void mezclar(circuito* circuito1, circuito* circuito2, unsigned int& tam) {
+void introduceEnSet(std::set<circuito*> &set, circuito* circ) {
+	if (circ != nullptr) {
+		set.insert(circ);
+		introduceEnSet(set, circ->padreIzq);
+		introduceEnSet(set, circ->padreDer);
+	}
+}
 
+void mezclar(circuito* circuito1, circuito* circuito2, int& tam) {
+
+	std::set<circuito*> set1, set2;
+	introduceEnSet(set1, circuito1);
+	introduceEnSet(set2, circuito2);
+
+	std::set<circuito*> intersect;
+	std::set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), std::inserter(intersect, intersect.begin()));
+
+	for (circuito* circ : intersect) {
+		if (circ->tam > 0) tam--;
+	}
+
+	/*
 	if (circuito1 == nullptr || circuito2 == nullptr) return;
 
 	if (circuito1->prof > circuito2->prof) {
@@ -125,13 +178,16 @@ void mezclar(circuito* circuito1, circuito* circuito2, unsigned int& tam) {
 		mezclar(circuito1, circuito2->padreIzq, tam);
 		mezclar(circuito1, circuito2->padreDer, tam);
 	}
-	else if (circuito1 == circuito2) tam -= circuito1->tam;
+	else if (circuito1 == circuito2) {
+		tam -= circuito1->tam;
+	}
 	else {
 		mezclar(circuito1->padreIzq, circuito2->padreIzq, tam);
 		mezclar(circuito1->padreIzq, circuito2->padreDer, tam);
 		mezclar(circuito1->padreDer, circuito2->padreIzq, tam);
 		mezclar(circuito1->padreDer, circuito2->padreDer, tam);
 	}
+	*/
 }
 
 // Funcion que totalmente ad-hoc pone los circuitos detamaño 0 (las entradas) con sus correspondientes evaluaciones 
@@ -156,52 +212,48 @@ void inicializa() {
 }
 
 unsigned int evaluador(puerta gate, circuito* circuito1, circuito* circuito2) {
-	if (gate == AND) {
-		std::cout << "And de ";
-		bin(std::cout, circuito1->eval);
-		std::cout << " ";
-		bin(std::cout, circuito2->eval);
-		unsigned int result = circuito1->eval & circuito2->eval;
-		std::cout << " resultado ";
-		bin(std::cout, result);
-		std::cout << "\n";
-		return result;
-	}
-	else if (gate == OR) {
-		std::cout << "Or de ";
-		bin(std::cout, circuito1->eval);
-		std::cout << " ";
-		bin(std::cout, circuito2->eval);
-		unsigned int result = circuito1->eval | circuito2->eval;
-		std::cout << " resultado ";
-		bin(std::cout, result);
-		std::cout << "\n";
-		return result;
-	}
-	else if (gate == NOT) {
-		std::cout << "Not de ";
-		bin(std::cout, circuito1->eval);
-		unsigned int result = ~circuito1->eval;
-		std::cout << " resultado ";
-		bin(std::cout, result);
-		std::cout << "\n";
-		return result;
+	switch (gate)
+	{
+	case AND:
+		return circuito1->eval & circuito2->eval;
+	case OR:
+		return circuito1->eval | circuito2->eval;
+	case NOT:
+		return ~circuito1->eval;
+	case NAND:
+		return ~(circuito1->eval & circuito2->eval);
+	case NOR:
+		return ~(circuito1->eval | circuito2->eval);
+	case XOR:
+		return circuito1->eval ^ circuito2->eval;
+	case XNOR:
+		return ~(circuito1->eval ^ circuito2->eval);
+	case LONLY:
+		return circuito1->eval & (~circuito2->eval);
+	case RONLY:
+		return (~circuito1->eval) & circuito2->eval;
+	case NLONLY:
+		return ~(circuito1->eval & (~circuito2->eval));
+	case NRONLY:
+		return ~((~circuito1->eval) & circuito2->eval);
+	default:
+		break;
 	}
 }
 
 
-bool generaCircuitos(unsigned int n) {
+bool generaCircuitos(int n) {
 
 	if (!espera.count(n)) return true;
 
 
-	for (puerta gate : repertorioBBinario) {
-		for (int i = 0; i < espera[n].size(); ++i) {
+	for (puerta gate : repertorioABinario) {
+		for (size_t i = 0; i < espera[n].size(); ++i) {
 			circuito* circuito1 = espera[n][i];
 
-			for (int j = i + 1; j < espera[n].size(); ++j) {
+			for (size_t j = i + 1; j < espera[n].size(); ++j) {
 				circuito* circuito2 = espera[n][j];
-				unsigned int tam = circuito1->tam + circuito2->tam + 1;
+				int tam = circuito1->tam + circuito2->tam + 1;
 				mezclar(circuito1, circuito2, tam);
 				if (!espera.count(tam)) espera[tam] = std::vector<circuito*>();
 				espera[tam].push_back(new circuito(gate, circuito1, circuito2, evaluador(gate, circuito1, circuito2), tam, std::max(circuito1->prof, circuito2->prof) + 1));
@@ -213,7 +265,7 @@ bool generaCircuitos(unsigned int n) {
 			for (auto par2 : almacen) {
 				for (circuito* circuito2 : par2.second) {
 					if (circuito1 == circuito2) break;
-					unsigned int tam = circuito1->tam + circuito2->tam + 1;
+					int tam = circuito1->tam + circuito2->tam + 1;
 					mezclar(circuito1, circuito2, tam);
 					if (!espera.count(tam)) espera[tam] = std::vector<circuito*>();
 					espera[tam].push_back(new circuito(gate, circuito1, circuito2, evaluador(gate, circuito1, circuito2), tam, std::max(circuito1->prof, circuito2->prof) + 1));
@@ -224,9 +276,9 @@ bool generaCircuitos(unsigned int n) {
 		}
 	}
 
-	for (puerta gate : repertorioBUnario) {
+	for (puerta gate : repertorioAUnario) {
 		for (circuito* circuito1 : espera[n]) {
-			unsigned int tam = circuito1->tam + 1;
+			int tam = circuito1->tam + 1;
 			if (!espera.count(tam)) espera[tam] = std::vector<circuito*>();
 			espera[tam].push_back(new circuito(gate, circuito1, nullptr, evaluador(gate, circuito1, nullptr), tam, circuito1->prof + 1));
 			circuitosGenerados++;
@@ -245,7 +297,7 @@ void anotaCircuitos(int n) {
 		}
 		almacen[circ->eval].push_back(circ);
 	}
-	std::cout << "Tamano " << n << " anotado\n";
+	std::cout << "Size " << n << " anotado\n";
 
 }
 
@@ -255,11 +307,20 @@ inline void liberaCircuitos(int n) {
 }
 
 inline void anotaSobrantes() {
-	for (auto par : espera) {
-		anotaCircuitos(par.first);
-	}
+	std::vector<std::pair<int, std::vector<circuito*>>> sobrantes(espera.size());
+	std::transform(espera.begin(), espera.end(), sobrantes.begin(), [](auto pair) {return move(pair);});
 	espera.clear();
-
+	sort(sobrantes.begin(), sobrantes.end(), [](auto pair1, auto pair2) {return pair1.first < pair2.first; });
+	for (auto par : sobrantes) {
+		for (circuito* circ : par.second) {
+			if (!almacen.count(circ->eval)) {
+				almacen[circ->eval] = std::vector<circuito*>();
+				almacen[circ->eval].push_back(circ);
+				fs << circ;
+			}
+		}
+		std::cout << "Size " << par.first << " anotado\n";
+	}
 }
 
 inline void muestraSobrantes() {
@@ -270,7 +331,7 @@ inline void muestraSobrantes() {
 
 int main() {
 	inicializa();
-	unsigned int tamano = 0;
+	int tamano = 0;
 	while (generaCircuitos(tamano)) {
 		anotaCircuitos(tamano);
 		liberaCircuitos(tamano);
@@ -284,7 +345,7 @@ int main() {
 	std::cout << "Procesando... \n";
 	anotaSobrantes();
 	std::cout << "Total de funciones distintas computadas " << almacen.size() << '\n';
+	fs << "Total de funciones distintas computadas: " << almacen.size() << '\n';
 	fs.close();
-
 	return 0;
 }
