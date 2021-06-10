9109d650f95d16042ba39886b63610f301da04b1
Jorge <georgeatletico@gmail.com>
2021-06-10 23:01:49 +0200

    reestructuración en carpetas

---
+++ CliqueYParidad/paridad.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+bool parUnos(int num, int n) {
+	int unos = 0;
+	for (int i = 0; i < n; ++i) {
+		if (num % 2 == 1) unos++;
+		num /= 2;
+	}
+	return unos % 2 == 0;
+}
+int main() {
+	ofstream salidaParidad("salidaParidad.txt");
+	int n = 21;
+	for (int num = 0; num < pow(2, n); ++num) {
+		if (parUnos(num, n)) salidaParidad << '1';
+		else salidaParidad << '0';
+	}
+	return 0;
+}
